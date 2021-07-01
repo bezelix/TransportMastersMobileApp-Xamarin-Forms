16bfa8575c9a4bfb53a9efa9142a00a0f574dac8
bezelix <novusitpl@gmail.com>
2021-07-01 12:03:34 +0200

    Add project files.

---
+++ TransportMasters/TransportMasters/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using TransportMasters.ViewModels;
+using Xamarin.Forms;
+
+namespace TransportMasters.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ TransportMasters/TransportMasters/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using TransportMasters.Models;
+using TransportMasters.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TransportMasters.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
